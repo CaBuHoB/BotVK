879009c65fe354a8288884b8b30f73479ca66c4b
CaBuHOB <cabuhob@gmail.com>
2018-07-01 16:48:06 +0300

    Токен от тестовой группы

--- Bot/Basis/Main.py
+++ Bot/Basis/Main.py
@@ -6,6 +6,7 @@ import time
 from Bot.Basis import MessageReplay
 
 api_token = '890e1e0743f9afdcf2787f6338c1fd0bc73327a2aa398d8cd38d4e6fdb998b08b43f0a26b905bf25ae47b'
+api_token = '07bad0077791b970f09942de845145ae326dc6c6b3d89c03690b1240f4a4a033899cf96e5fa72d6a334bb' ##тестовый
 vkApi = vk_api.VkApi(token=api_token)
 vk = vkApi.get_api()
 
@@ -18,4 +19,4 @@ while True:
     for item in response['items']:
         my_thread = MessageReplay.MessageReplay(vkApi, item)
         my_thread.start()
-    time.sleep(1)
+    time.sleep(0.5)
