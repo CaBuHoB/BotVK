6f885fa0bc0209756d85ab7f16241b49c5c432ff
CaBuHoB <cabuhob@gmail.com>
2018-09-16 19:28:39 +0300

    Изменил файл конфигов

--- Bot/Basis/Configs.py
+++ Bot/Basis/Configs.py
@@ -3,8 +3,13 @@ import vk
 from Bot.Basis.DataBase.workWithDataBase import getConnect, getAllUsers
 from Bot.Basis.Timetable.getSchedule import getDate, getTimetableDict
 
-token = '07bad0077791b970f09942de845145ae326dc6c6b3d89c03690b1240f4a4a033899cf96e5fa72d6a334bb'
-confirmation_token = 'e10e7673'
+# Тестовый бот
+# token = '07bad0077791b970f09942de845145ae326dc6c6b3d89c03690b1240f4a4a033899cf96e5fa72d6a334bb'
+# confirmation_token = 'e10e7673'
+
+# Основной бот
+token = '890e1e0743f9afdcf2787f6338c1fd0bc73327a2aa398d8cd38d4e6fdb998b08b43f0a26b905bf25ae47b'
+confirmation_token = 'c9a8cdcb'
 
 connect = getConnect()
 users = getAllUsers(connect)
