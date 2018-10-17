28c5252f0b3c4729826c865837641c7119aec790
CaBuHoB <cabuhob@gmail.com>
2018-10-18 00:45:38 +0300

    Добавил расширенный алгоритм Евклида

--- Bot/Basis/Configs.py
+++ Bot/Basis/Configs.py
@@ -9,7 +9,7 @@ from Bot.Basis.Functions.getWeatherForecast import getWeather
 
 # Тестовый бот
 # token = '07bad0077791b970f09942de845145ae326dc6c6b3d89c03690b1240f4a4a033899cf96e5fa72d6a334bb'
-# confirmation_token = 'b3451e9c'
+# confirmation_token = 'e10e7673'
 
 # Основной бот
 token = '890e1e0743f9afdcf2787f6338c1fd0bc73327a2aa398d8cd38d4e6fdb998b08b43f0a26b905bf25ae47b'
