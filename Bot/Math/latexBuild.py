9968d68a6e0850d5e18e4e22243811e16594cd17
Maxim <cabuhob@gmail.com>
2018-11-26 02:17:04 +0300

    Добавил список пакетов для texlive

--- Bot/Math/latexBuild.py
+++ Bot/Math/latexBuild.py
@@ -17,7 +17,7 @@ def getPreamble():
             r"\usepackage{makeidx}"
             r"\usepackage{graphicx}"
             r"\usepackage{multicol}"
-            r"\usepackage[english,russianb]{babel}"
+            r"\usepackage[english,russian]{babel}"
             r""
             r"\begin{document}")
 
