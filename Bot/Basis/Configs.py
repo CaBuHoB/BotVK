e347706edc5e35ef22c298ad53de4da986f2ec6b
Maxim <cabuhob@gmail.com>
2018-11-27 21:14:33 +0300

    Убрал лишние потоки

--- Bot/Basis/Configs.py
+++ Bot/Basis/Configs.py
@@ -5,9 +5,6 @@ import vk
 
 from Bot.Basis.Functions.getSchedule import getTimetableDict
 from Bot.Basis.Functions.getWeatherForecast import getWeather
-from Bot.Basis.Threads.QueueThread import QueueThread
-from Bot.Basis.Threads.TimetableNotifications import TimetableNotifications
-from Bot.Basis.Threads.WeatherThread import WeatherThread
 
 token = os.environ['TOKEN']
 confirmation_token = os.environ['CONFIRMATION_TOKEN']
@@ -20,15 +17,3 @@ isUpper = True if (datetime.now().isocalendar()[1] % 2 == 0) else False
 
 session = vk.Session(token)
 api = vk.API(session, v=5.85)
-
-api.messages.send(user_id=38081883, message='Бот обновился (:')
-api.messages.send(user_id=88195126, message='Бот обновился :)')
-
-notifications_thread = TimetableNotifications(api, timetableDict)
-notifications_thread.start()
-
-queue_thread = QueueThread(api)
-queue_thread.start()
-
-weather_thread = WeatherThread(api)
-weather_thread.start()
